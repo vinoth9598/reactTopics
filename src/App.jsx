68c87b3d3aa5f983556297dd6369b527b1d9c73c
vinoth9598 <pvinoth9598@gmail.com>
2026-05-08 16:01:24 +0530

    initial react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react" ;
+
+function App(){
+  return(
+    <div>
+        <h1>Welcome to React</h1>
+        <p>This comment use it for create react App</p>
+        <b>npm create vite@latest project_fileName</b>
+    </div>
+  )
+}
+
+export default App ;
