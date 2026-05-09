18961b9b393e576c1130effa11f0b797b93931ae
vinoth9598 <pvinoth9598@gmail.com>
2026-05-09 12:47:27 +0530

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 
 import React from "react" ;
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Notes from "./Components/Notes";
+import Users from "./Components/Users";
 
 function App(){
+  const padding = {
+    padding:5 ,
+  }
   return(
-    <div>
-        <h1>Welcome to React</h1>
-        <p>This comment use it for create react App</p>
-        <b>npm create vite@latest project_fileName</b>
-    </div>
+    <Router>
+      <div>
+        <Link to='/' style={padding}>Home</Link>
+        <Link to='/notes' style={padding}>Notes</Link>
+        <Link to='/users' style={padding}>Users</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/notes" element={<Notes/>}/>
+        <Route path="/users" element={<Users/>}/>
+      </Routes>
+    </Router>
   )
 }
 
