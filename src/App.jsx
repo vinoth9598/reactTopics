9cf49951a7947d9b9a0254e3fd189143b07010c2
vinoth9598 <pvinoth9598@gmail.com>
2026-05-11 12:31:29 +0530

    useContext hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+// Context API: It is to manage and share the states across the nested components or component tree without need to pass the props data down through multiple levels of the nested components.
 
-import React from "react" ;
+import React, { createContext, useState } from "react" ;
+import ChildComponent from "./Components/ChildComponent";
+//create a context for sharing the message data
+const MessageContext = createContext();
 
 function App(){
+  //this message should be rendered inside the grandchild component 
+
+  const [message, setMessage] = useState('Hello , Grand Child');
+  const [coins, setCoins] = useState([1, 2, 3, 4, 5]);
+
   return(
     <div>
-        <h1>Welcome to React</h1>
-        <p>This comment use it for create react App</p>
-        <b>npm create vite@latest project_fileName</b>
+       <h1>Parent Component</h1>
+       <hr/>
+       <MessageContext.Provider value={{message, coins}}>
+          <ChildComponent/>
+       </MessageContext.Provider>
     </div>
   )
 }
 
-export default App ;
+export { App as default , MessageContext};
