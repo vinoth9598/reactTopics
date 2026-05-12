3fcd6221adb78a4b31f66701a06e05a59e52bc36
vinoth9598 <pvinoth9598@gmail.com>
2026-05-12 16:26:06 +0530

    simple example of redux

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import React from "react" ;
 function App(){
   return(
     <div>
-        <h1>Welcome to React</h1>
-        <p>This comment use it for create react App</p>
-        <b>npm create vite@latest project_fileName</b>
+        <p>Simple Redux</p>
     </div>
   )
 }
