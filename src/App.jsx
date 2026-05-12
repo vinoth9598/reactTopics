d400e96ea79a519703d32c8de827c336e06584e6
vinoth9598 <pvinoth9598@gmail.com>
2026-05-12 16:48:53 +0530

    react redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 
 import React from "react" ;
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { createStore } from "redux";
 
+// reducer 
+const reducer = (state = {count : 0}, action) => {
+  switch (action.type) {
+    case "Incr" :
+      return {count : state.count + 1} ;
+    case "Decr" :
+      return {count : state.count - 1} ;
+    default :
+      return state ;
+  }
+};
+
+// Store 
+const store = createStore(reducer);
+
+
+// function counter 
+
+function Counter(){
+  const count = useSelector((state) => state.count);
+
+  const dispatch = useDispatch() ;
+
+  return (
+    <div style={{ textAlign:"center", marginTop:"50px"}}>
+      <h1>React Redux Example</h1>
+      <h2>count : {count}</h2>
+      <button onClick={()=> dispatch({type:"Incr"})}>Increment</button>
+      <button onClick={()=> dispatch({type:"Decr"})} style={{marginLeft:"10px"}}>Decrement</button>
+    </div>
+  )
+}
 function App(){
   return(
-    <div>
-        <p>Simple Redux</p>
-    </div>
+    <Provider store = {store}>
+        <Counter />
+    </Provider>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,33 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client' ;
 import App from './App.jsx' ;
 
-import { createStore } from 'redux';
-
-const counterReducer = (state = 0, action) => {
-    console.log(action) ;
-
-    switch(action.type) {
-        case 'Incr' :
-            return state + 1 ;
-        case 'Decr' :
-            return state - 1 ;
-        default :
-            return state ;
-    }
-}
-
-//create a store 
-const store = createStore(counterReducer);
-
-//get the current store state 
-console.log(store.getState());
-
-//update action
-store.dispatch({
-    type : 'Incr'
-})
-console.log(store.getState());
-
 createRoot(document.getElementById('root')).render(
     <App />
 );
