7f24cb9ae4d9ca25d2adb213d19cf82e4f245549
vinoth9598 <pvinoth9598@gmail.com>
2026-05-12 15:57:17 +0530

    formik demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
 
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react" ;
+
+const ContactForm = () => {
+  return (
+     <Formik
+      initialValues={{name : "", email:"", message:""}}
+
+      validate={(values)=> {
+        const errors = {} ;
+        if(!values.name){
+          errors.name = "Name is Required" ;
+        }
+
+        if(values.email == ''){
+          errors.email = 'Email is required' ;
+        }else if(!/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/i.test(values.email)){
+          errors.email = "Invalid email address" ;
+        }
+        return errors ;
+      }}
+    >
+      <Form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <Field type='text' name='name'/>
+          <ErrorMessage name='name' component='div' className="error"/>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field type='email' name='email'/>
+          <ErrorMessage name="email" component='div' className="error"/>
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <Field as='textarea' name='message'/>
+          <ErrorMessage name='message' component='div' className="error" />
+        </div>
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  )
+}
 
 function App(){
   return(
-    <div>
-        <h1>Welcome to React</h1>
-        <p>This comment use it for create react App</p>
-        <b>npm create vite@latest project_fileName</b>
-    </div>
+   <div>
+      <ContactForm/>
+   </div>
   )
 }
 
